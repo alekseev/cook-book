0fb4f969d2c588d3d3625467e5c7e0db8a016ef4
Denis Alekseev <alekseevdy@gmail.com>
2011-06-20 17:46:59 +0400

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//почитать про link_to_function http://apidock.com/rails/ActionView/Helpers/JavaScriptHelper/link_to_function
+function destroy() {
+  alert('!');
+  return false;
+}
